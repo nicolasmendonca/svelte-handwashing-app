1ad179d196d80a8ce00adbc68e4eb3b3b667920f
Nicolás Mendonca <ae_nmendonca@idcontractors.net>
2021-10-19 21:26:55 -0300

    Adds Lato font and blueprintcss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@500&display=swap');
+  @import url('blueprint-css/dist/blueprint.min.css');
+
+  :global(body) {
+    font-family: 'Lato', sans-serif;
+  }
+</style>
+
+<slot></slot>
